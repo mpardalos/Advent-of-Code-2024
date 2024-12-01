44f2ae38c74c7e97a3a20b38dd73eff2ead65efe
Michalis Pardalos <me@mpardalos.com>
2024-12-01 11:42:13 +0000

    Day 1

--- solutions/Day1.hs
+++ solutions/Day1.hs
@@ -1,9 +1,29 @@
 module Day1 (part1, part2) where
 
+import Data.Bifunctor (bimap)
 import Data.ByteString (ByteString)
+import Data.ByteString.Char8 qualified as BS
+import Data.Function ((&))
+import Data.IntMultiSet qualified as IntMultiSet
+import Data.List (sort)
 
-part1 :: ByteString -> ()
-part1 _ = ()
+readVerticalLists :: ByteString -> ([Int], [Int])
+readVerticalLists input =
+  BS.lines input
+    & map BS.words
+    & map (\[l, r] -> (l, r))
+    & map (bimap (read . BS.unpack) (read . BS.unpack))
+    & unzip
 
-part2 :: ByteString -> ()
-part2 _ = ()
+part1 :: ByteString -> Int
+part1 input =
+  readVerticalLists input
+    & bimap sort sort
+    & uncurry (zipWith ((abs .) . subtract))
+    & sum
+
+part2 :: ByteString -> Int
+part2 input =
+  let (l, r) = bimap sort sort $ readVerticalLists input
+      multiset_right = IntMultiSet.fromList r
+   in sum [n * IntMultiSet.occur n multiset_right | n <- l]
