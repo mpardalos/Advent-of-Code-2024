554dbdecbe9bc68ea9250d48f15b0376cbb921ba
Michalis Pardalos <me@mpardalos.com>
2024-12-01 12:09:48 +0000

    Format Util

--- solutions/Util.hs
+++ solutions/Util.hs
@@ -33,10 +33,10 @@ parseOrError parser input = case parseOnly parser input of
   Left e -> error ("Parse error: " <> show e)
   Right v -> v
 
-hashNub :: Hashable a => [a] -> [a]
+hashNub :: (Hashable a) => [a] -> [a]
 hashNub = HashSet.toList . HashSet.fromList
 
-ordNub :: Ord a => [a] -> [a]
+ordNub :: (Ord a) => [a] -> [a]
 ordNub = Set.toList . Set.fromList
 
 readIntsSepBy :: Char -> ByteString -> [Int]
@@ -68,10 +68,10 @@ uniquePairs :: [a] -> [(a, a)]
 uniquePairs [] = []
 uniquePairs (x : xs) = [(x, y) | y <- xs] ++ uniquePairs xs
 
-manhattanDistance :: Num a => (a, a) -> (a, a) -> a
+manhattanDistance :: (Num a) => (a, a) -> (a, a) -> a
 manhattanDistance (row1, col1) (row2, col2) = abs (row2 - row1) + abs (col2 - col1)
 
-traceShowIdLabelled :: Show a => String -> a -> a
+traceShowIdLabelled :: (Show a) => String -> a -> a
 traceShowIdLabelled l x = trace (l ++ ": " ++ show x) x
 
 graphvizView :: String -> IO ProcessHandle
