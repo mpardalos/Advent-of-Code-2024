f74339c2275d570c032452714db7655973ca1074
Michalis Pardalos <me@mpardalos.com>
2024-12-02 17:37:38 +0000

    Day 2

--- solutions/Day2.hs
+++ solutions/Day2.hs
@@ -1,9 +1,41 @@
 module Day2 (part1, part2) where
 
 import Data.ByteString (ByteString)
+import qualified Data.ByteString.Char8 as BS
+import Data.Function ((&))
+import Util (readSpacedInts)
 
-part1 :: ByteString -> ()
-part1 _ = ()
+allIncreasing :: [Int] -> Bool
+allIncreasing (x1:x2:xs) = x2 > x1 && allIncreasing (x2:xs)
+allIncreasing _ = True
 
-part2 :: ByteString -> ()
-part2 _ = ()
+allDecreasing :: [Int] -> Bool
+allDecreasing (x1:x2:xs) = x2 < x1 && allDecreasing (x2:xs)
+allDecreasing _ = True
+
+differencesSafe :: [Int] -> Bool
+differencesSafe (x1:x2:xs) =
+  let diff = abs (x2 - x1)
+  in (1 <= diff && diff <= 3) && differencesSafe (x2:xs)
+differencesSafe _ = True
+
+isSafe :: [Int] -> Bool
+isSafe xs = (allIncreasing xs || allDecreasing xs) && differencesSafe xs
+
+dropping1 :: [Int] -> [[Int]]
+dropping1 [] = [[]]
+dropping1 (x:xs) = xs : map (x:) (dropping1 xs)
+
+part1 :: ByteString -> Int
+part1 input =
+  BS.lines input
+  & map readSpacedInts
+  & filter isSafe
+  & length
+
+part2 :: ByteString -> Int
+part2 input =
+  BS.lines input
+  & map readSpacedInts
+  & filter (any isSafe . dropping1)
+  & length
