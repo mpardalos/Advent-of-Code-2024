962e5ef916f1d3b17785e6e265ab41eb4ab84465
Michalis Pardalos <me@mpardalos.com>
2024-12-11 09:48:55 +0000

    Day 11

--- solutions/Day11.hs
+++ solutions/Day11.hs
@@ -1,9 +1,43 @@
+{-# LANGUAGE MultiWayIf #-}
+
 module Day11 (part1, part2) where
 
 import Data.ByteString (ByteString)
+import Data.Function ((&))
+import Data.MemoTrie (memo2)
+import Math.NumberTheory.Logarithms (integerLog10')
+import Util (readSpacedInts)
 
-part1 :: ByteString -> ()
-part1 _ = ()
+splitDigits :: Int -> Maybe (Int, Int)
+splitDigits n =
+  let digitCount = integerLog10' (fromIntegral n) + 1
+      middleDigit = digitCount `div` 2
+   in if even digitCount
+        then Just (n `div` (10 ^ middleDigit), n `mod` (10 ^ middleDigit))
+        else Nothing
 
-part2 :: ByteString -> ()
-part2 _ = ()
+countAfter :: Int -> Int -> Int
+countAfter =
+  memo2 $ \steps x ->
+    if
+      | steps == 0 ->
+          1
+      | x == 0 ->
+          countAfter (steps - 1) 1
+      | Just (l, r) <- splitDigits x ->
+          countAfter (steps - 1) l
+            + countAfter (steps - 1) r
+      | otherwise ->
+          countAfter (steps - 1) (2024 * x)
+
+part1 :: ByteString -> Int
+part1 input =
+  readSpacedInts input
+    & map (countAfter 25)
+    & sum
+
+part2 :: ByteString -> Int
+part2 input =
+  readSpacedInts input
+    & map (countAfter 75)
+    & sum
