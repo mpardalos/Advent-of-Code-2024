c17fd6c683758b2f74deae6c05f8bf1ef6639cc7
Michalis Pardalos <me@mpardalos.com>
2024-12-12 23:38:36 +0000

    Parallel runner

--- Main.hs
+++ Main.hs
@@ -1,43 +1,43 @@
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE ApplicativeDo #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ExtendedDefaultRules #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE NumDecimals #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# OPTIONS_GHC -Wno-type-defaults #-}
 
 module Main where
 
 import Control.Exception (Exception, SomeException, catch, evaluate, throwIO, try)
-import Control.Monad (forM, when, foldM)
+import Control.Monad (forM, when, filterM, replicateM_)
 import Data.ByteString (ByteString)
 import Data.ByteString.Char8 qualified as BS
 import Data.Function ((&))
-import Data.List (isInfixOf, foldl')
 import Data.Time.Calendar
 import Data.Time.Clock
 import Debug.Trace (traceMarkerIO)
 import Network.HTTP.Simple
 import Network.HTTP.Types
-import Solutions (Solution (..), displayAnswer, isSolvedAnswer, problemName, solutions, IsSlow(..))
+import Options.Applicative
+import Solutions (IsSlow (..), Solution (..), displayAnswer, isSolvedAnswer, problemName, solutions)
 import System.Clock
 import System.Directory
-import System.Environment (getArgs, getEnv)
-import System.Exit (exitFailure)
+import System.Environment (getEnv)
 import System.IO
 import Text.Printf (printf)
+import Control.Concurrent (newMVar, withMVar, newQSem, signalQSem, waitQSem, QSem)
+import System.Console.ANSI (cursorUp, saveCursor, restoreCursor, cursorUpLine, setCursorColumn)
+import Control.Concurrent.Async (async, wait)
 
 default (String)
 
 titleLength :: Int
 titleLength =
   maximum (map (length . problemName) solutions)
-
-data AnchorType
-  = Top
-  | Middle
-  | Bottom
 
 printTableAnchor :: AnchorType -> IO ()
 printTableAnchor anchorType =
@@ -85,38 +85,50 @@ printLineAnswer time answer =
       | timeNanos == 0 = ""
       | otherwise = printf "%.3f ms" (timeNanos / 1e6)
 
+data AnchorType
+  = Top
+  | Middle
+  | Bottom
+
 data AOCError = InputNotOutYet
-  deriving (Show)
   deriving anyclass (Exception)
 
-currentAOCYear :: IO Year
-currentAOCYear = do
-  currentTime <- getCurrentTime
-  let (year, month, _) = toGregorian $ utctDay currentTime
+instance Show AOCError where
+  show InputNotOutYet = "Input not out yet"
+
+getAOCYear :: IO Year
+getAOCYear =
+  (read <$> getEnv "AOC_YEAR")
+    `catch` ( \(_ :: IOError) -> do
+                currentTime <- getCurrentTime
+                let (year, month, _) = toGregorian $ utctDay currentTime
+                return $
+                  if month >= 12
+                    then year
+                    else year - 1
+            )
+
+solutionInputAvailable :: Int -> IO Bool
+solutionInputAvailable day = do
+  (currentYear, currentMonth, currentDay) <- toGregorian . utctDay <$> getCurrentTime
+  aocYear <- getAOCYear
+
   return $
-    if month >= 12
-      then year
-      else year - 1
+    if
+      | currentYear < aocYear -> False
+      | currentYear > aocYear -> True
+      | currentMonth < 12 -> False
+      | currentDay < day -> False
+      | otherwise -> True
 
 -- | Get input for a given day. The year is set by the AOC_YEAR environment
 -- variable, or the current year if that is not set
 downloadInput :: Int -> IO ByteString
 downloadInput inputDay = do
-  (currentYear, currentMonth, currentDay) <- toGregorian . utctDay <$> getCurrentTime
-  aocYear <- (read <$> getEnv "AOC_YEAR") `catch` (\(_ :: IOError) -> currentAOCYear)
-
-  let inputIsAvailable
-        | currentYear < aocYear = False
-        | currentYear > aocYear = True
-        | currentMonth < 12 = False
-        | currentDay < inputDay = False
-        | otherwise = True
-
-  when (not inputIsAvailable) (throwIO InputNotOutYet)
-
   hPutStrLn stderr ("Downloading input for day " <> show inputDay)
 
   session_cookie <- getEnv "AOC_SESSION_COOKIE"
+  aocYear <- getAOCYear
 
   let request =
         parseRequest_ (printf "https://adventofcode.com/%d/day/%d/input" aocYear inputDay)
@@ -136,63 +148,122 @@ downloadInput inputDay = do
   return (getResponseBody response)
 
 -- | Get input for a given day. If it is not downloaded, download it using @downloadInput@
-getInput :: Int -> IO ByteString
-getInput day =
-  BS.readFile inputPath `catch` \(_ :: IOError) -> do
-    downloadedInput <- downloadInput day
-    createDirectoryIfMissing False "data/"
-    BS.writeFile inputPath downloadedInput
-    -- Read the file to make sure that we get any possible issues with the file now, and not the next time it is read.
-    BS.readFile inputPath
+getInput :: Int -> IO (Either SomeException ByteString)
+getInput day = do
+  available <- solutionInputAvailable day
+  try $ do
+    when (not available) (throwIO InputNotOutYet)
+
+    BS.readFile inputPath `catch` \(_ :: IOError) -> do
+      downloadedInput <- downloadInput day
+      createDirectoryIfMissing False "data/"
+      BS.writeFile inputPath downloadedInput
+      -- Read the file to make sure that we get any possible issues with the file now, and not the next time it is read.
+      BS.readFile inputPath
   where
     inputPath = "data/day" <> show day
 
+withTiming :: String -> IO a -> IO (TimeSpec, a)
+withTiming label a = do
+  startTime <- getTime Monotonic
+  traceMarkerIO ("Begin " ++ label)
+  out <- a
+  traceMarkerIO ("End " ++ label)
+  timeElapsed <- diffTimeSpec startTime <$> getTime Monotonic
+  return (timeElapsed, out)
+
+runSolution :: Solution -> IO (TimeSpec, String)
+runSolution solution@MkSolution {..} =
+  getInput day >>= \case
+    Left exc ->
+      return (0, show exc)
+    Right input -> do
+      (timeElapsed, answer) <-
+        withTiming (problemName solution) . try . evaluate $
+          solve input
+      case answer of
+        Left (e :: SomeException) -> return (0, show e)
+        Right v
+          | isSolvedAnswer v -> return (timeElapsed, displayAnswer v)
+          | otherwise -> return (0, "")
+
+data Options = MkOptions
+  { solutionFilter :: Solution -> Bool
+  , showFuture :: Bool
+  , sequential :: Bool
+  }
+
+options :: Parser Options
+options = do
+  skipSlow <-
+    asum
+      [ flag'
+          (\s -> s.isSlow /= Slow)
+          (long "noslow" <> help "Skip slow solutions"),
+        flag'
+          (\s -> s.isSlow == Slow)
+          (long "slow" <> help "Only run slow solutions"),
+        pure (const True)
+      ]
+  day <-
+    asum
+      [ option
+          ( do {day <- auto; return (\s -> s.day == day)} )
+          (long "day" <> short 'd' <> help "Only run this day" <> metavar "DAY"),
+        pure (const True)
+      ]
+  showFuture <- switch (long "show-future" <> help "Show days that are not available yet")
+  sequential <- switch (long "sequential" <> help "Run solutions one-by-one")
+  pure (let solutionFilter s = skipSlow s && day s in MkOptions {..})
+
+parseArgs :: IO Options
+parseArgs =
+  execParser $
+    info
+      (options <**> helper)
+      (progDesc "Michalis' solutions to Advent of Code 2024")
+
 main :: IO ()
 main = do
-  args <- getArgs
-  let filters = map (\case
-                        "--notslow" -> \solution -> solution.isSlow == NotSlow
-                        "--slow" -> \solution -> solution.isSlow == Slow
-                        str -> \solution -> str `isInfixOf` problemName solution)
-                args
-  let filterFun = foldl' (\f1 f2 s -> f1 s && f2 s) (const True) filters
+  MkOptions {solutionFilter, showFuture, sequential} <- parseArgs
 
-  solutionsWithInputs <- forM (filter filterFun solutions) $ \solution -> do
-    ( do
-        input <- getInput solution.day
-        return (solution, Right input)
-      )
-      `catch` (\InputNotOutYet -> return (solution, Left "    (Input not yet available)"))
-      `catch` (\(e :: SomeException) -> return (solution, Left (show e)))
+  toRun <- solutions
+    & filter solutionFilter
+    & filterM (\MkSolution {day} -> (showFuture ||) <$> solutionInputAvailable day)
 
-  printTableAnchor Top
-  times <- forM solutionsWithInputs $ \case
-    (solution, Left msg) -> do
+  -- Make space to fit the lines that will be printed
+  replicateM_ (length toRun + 5) $ putStrLn ""
+  cursorUp (length toRun + 5)
+
+  ioLock <- newMVar ()
+  let withIOLock = withMVar ioLock . const
+  runLock <- newMVar ()
+  let withRunLock
+        | sequential = withMVar runLock . const
+        | otherwise = id
+
+  tasks <- withMVar ioLock $ \() -> do
+    printTableAnchor Top
+    tasks <- forM (zip [0..] toRun) $ \(idx, solution) -> do
       printLineName (problemName solution)
-      printLineAnswer 0 msg
-      return 0
-    (solution@MkSolution {solve}, Right input) -> do
-      let name = problemName solution
-      printLineName name
-      startTime <- getTime Monotonic
-      traceMarkerIO ("Begin " ++ name)
-      answer <-
-        try (evaluate (solve input)) >>= \case
-          Left (e :: SomeException) -> pure (Just (show e))
-          Right v
-            | isSolvedAnswer v -> pure (Just (displayAnswer v))
-            | otherwise -> pure Nothing
-      traceMarkerIO ("End " ++ name)
-      timeElapsed <- diffTimeSpec startTime <$> getTime Monotonic
-      case answer of
-        Nothing -> do
-          printLineAnswer 0 ""
-          return 0
-        Just s -> do
-          printLineAnswer timeElapsed s
-          return timeElapsed
+      putStrLn ""
+      return $ do
+        (timeElapsed, answer) <- withRunLock $
+          runSolution solution
+        withIOLock $ do
+          saveCursor
+          cursorUpLine (length toRun - idx + 1)
+          setCursorColumn (titleLength + 5)
+          printLineAnswer timeElapsed answer
+          restoreCursor
+        return timeElapsed
+    printTableAnchor Middle
+    return tasks
 
-  printTableAnchor Middle
+  times <- if sequential
+    then sequence tasks
+    else mapM wait =<< mapM async tasks
+
   printLineName "Total time"
   printLineAnswer (sum times) ""
   printTableAnchor Bottom
