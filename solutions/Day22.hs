e3d517e1b394099ef5ee48ec2b285404cadd1a85
Michalis Pardalos <me@mpardalos.com>
2025-01-02 00:33:50 +0000

    Day 22 part 1

--- solutions/Day22.hs
+++ solutions/Day22.hs
@@ -1,9 +1,35 @@
 module Day22 (part1, part2) where
 
+import Control.Applicative.Combinators (sepBy)
+import Data.Attoparsec.ByteString.Char8 (decimal, endOfLine)
+import Data.Bits (Bits (xor, (.&.)), (.<<.), (.>>.))
 import Data.ByteString (ByteString)
+import Data.Function ((&))
+import Data.Vector qualified as V
+import GHC.Exts (IsList (Item, fromList))
+import Util (parseOrError)
 
-part1 :: ByteString -> ()
-part1 _ = ()
+readInput :: (IsList l, Integral (Item l)) => ByteString -> l
+readInput = fromList . parseOrError (decimal `sepBy` endOfLine)
+
+mix :: Int -> Int -> Int
+mix = xor
+
+prune :: Int -> Int
+prune = (.&. (16777216 - 1))
+
+next :: Int -> Int
+next secret =
+  secret
+    & (\n -> prune $ mix n (n .<<. 6))
+    & (\n -> mix n (n .>>. 5))
+    & (\n -> prune $ mix n (n .<<. 11))
+
+part1 :: ByteString -> Int
+part1 input =
+  readInput input
+    & V.map ((!! 2000) . iterate next)
+    & sum
 
 part2 :: ByteString -> ()
 part2 _ = ()
